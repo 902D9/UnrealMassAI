5545c714eb2b1b63989ada7baaa418093db1e760
daizengtao <1711467513@qq.com>
2023-04-21 15:32:38 +0800

    first commit

---
+++ Source/MassAI/MassAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MassAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASSAI_API AMassAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
